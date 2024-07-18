578eb826c5ce566e0abd9245281d75d804a1f8c9
hananabo999 <139076368+hananabo999@users.noreply.github.com>
2024-07-19 00:29:18 +0300

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,50 +1,57 @@
 #!/usr/bin/python3
 """
-script that reads stdin line by line and computes metrics
+Read stdin line by line and computes metrics
+Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
+<status code> <file size>, skip line if not this format
+After every 10minutes or keyboard interrupt (CTRL + C)
+print these from beginning: number of lines by status code
+possible status codes: 200, 301, 400, 401, 404, 405, and 500
+if status code isn't an integer, do not print it
+format: <status code>: <number>
+Status code must be printed in ascending order
 """
-import signal
-import re
 import sys
 
 
-def happen(size, states_code):
-    """ happen """
-    print(f"File size: {size}")
-    for code in states_code:
-        if states_code[code] != 0:
-            print(f"{code}: {states_code[code]}")
+def print_msg(codes, file_size):
+    print("File size: {}".format(file_size))
+    for key, val in sorted(codes.items()):
+        if val != 0:
+            print("{}: {}".format(key, val))
 
 
-def main():
-    """ main function """
-    size = 0
-    states_code = {
-        "200": 0, "301": 0, "400": 0,
-        "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
-    counter = 1
-    try:
-        for line in sys.stdin:
-            elements = line.split(" ")
-            res = re.search(
-                r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|\w+)'
-                r'\s?-\s?\[\d{4}\-\d{2}\-\d{2}\s\d{2}\:'
-                r'\d{2}\:\d{2}\.\d+\]\s\"GET\s/projects/260\sHTTP/1.1\"'
-                r'\s(\d{1,3}|\w+)\s\d{1,4}$', line)
-            if res is not None:
-                size += int(elements[-1])
-                number = elements[-2]
-                if number in states_code:
-                    states_code[number] += 1
+file_size = 0
+code = 0
+count_lines = 0
+codes = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0
+}
 
-                if counter == 10:
-                    happen(size, states_code)
-                    counter = 0
-                counter += 1
-    except Exception as err:
-        pass
+try:
+    for line in sys.stdin:
+        parsed_line = line.split()
+        parsed_line = parsed_line[::-1]
 
-    finally:
-        happen(size, states_code)
+        if len(parsed_line) > 2:
+            count_lines += 1
 
+            if count_lines <= 10:
+                file_size += int(parsed_line[0])
+                code = parsed_line[1]
 
-main()
+                if (code in codes.keys()):
+                    codes[code] += 1
+
+            if (count_lines == 10):
+                print_msg(codes, file_size)
+                count_lines = 0
+
+finally:
+    print_msg(codes, file_size)
