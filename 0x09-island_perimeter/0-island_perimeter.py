e192be33a85f59f382ee1f892a1f1892c238c035
hananabo999 <139076368+hananabo999@users.noreply.github.com>
2024-09-02 02:27:28 +0300

    Create 0-island_perimeter.py

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" island perimeter """
+
+
+def island_perimeter(grid):
+    """ returns the perimeter of the island """
+    rows, cols = len(grid), len(grid[0])
+    perimeter = 0
+
+    for row in range(rows):
+        for col in range(cols):
+            if grid[row][col] == 1:
+                perimeter += 4
+                if col > 0 and grid[row][col - 1] == 1:
+                    perimeter -= 2
+                if row > 0 and grid[row - 1][col] == 1:
+                    perimeter -= 2
+    return perimeter
